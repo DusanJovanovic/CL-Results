1494aefe8cdcc32a08d3142b17445e6c9976c81b
DusanJovanovic <jovanovic.dusan@yahoo.com>
2020-08-16 00:37:31 +0200

    Fix app

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cl.sqlite3'
-db = SQLAlchemy(app)
+from cl_blueprints import results, tables
+from extensions import db
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def register_extensions(app):
+    db.init_app(app)
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        db.session.commit()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cl.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.register_blueprint(results)
+    app.register_blueprint(tables)
+
+    register_extensions(app)
+
+    return app
+
+app = create_app()
