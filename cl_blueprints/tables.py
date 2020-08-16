3de4e656bbfa53dda9df4f3912dbe4141e0512b7
DusanJovanovic <jovanovic.dusan@yahoo.com>
2020-08-16 23:47:40 +0200

    Add get tables endpoint

--- cl_blueprints/tables.py
+++ cl_blueprints/tables.py
@@ -1,6 +1,14 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from extensions import db
 from models import Result
+from utils import get_ranking
 
 tables = Blueprint("tables", __name__)
+
+@tables.route('/tables', methods=["GET"])
+def get_tables():
+    default_groups = [f"Group{x}" for x in "ABCDEFGH"]
+    groups = request.args.get('tables', default=default_groups, type=str)
+    tables = get_ranking(groups)
+    return jsonify(tables), 200
