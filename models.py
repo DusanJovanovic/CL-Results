4e8ccb9cf35a8b65f87e263f89ff8ab226ee6bcf
DusanJovanovic <jovanovic.dusan@yahoo.com>
2020-08-16 22:26:56 +0200

    Change primary key for result

--- models.py
+++ models.py
@@ -15,13 +15,13 @@ class Team(db.Model):
 class Result(db.Model):
 
     __tablename__ = 'result'
-    result_id = db.Column(db.Integer, primary_key=True)
     stage = db.Column(db.String(50), nullable=False)
     home_team = db.Column(db.String(200), nullable=False)
     away_team = db.Column(db.String(200), nullable=False)
     home_score = db.Column(db.Integer, nullable=False)
     away_score = db.Column(db.Integer, nullable=False)
     match_time = db.Column(db.DateTime, nullable=False)
+    match_uuid = db.Column(db.String(200), primary_key=True, nullable=False)
 
     #home_team_id = db.Column(db.Integer, db.ForeignKey('team.team_id'), nullable=False)
     #home_team = db.relationship('Team', backref=db.backref('team', lazy=True))
@@ -29,11 +29,11 @@ class Result(db.Model):
     #away_team = db.relationship('Team', backref=db.backref('team', lazy=True))
 
     def __repr__(self):
-        return f'<Result {self.result_id}>'
+        return f'<Result {self.match_uuid}>'
     
     def serialize(self):
         return {
-            'result_id': self.result_id,
+            'match_uuid': self.match_uuid,
             'stage': self.stage,
             'home_team': self.home_team,
             'away_team': self.away_team,
